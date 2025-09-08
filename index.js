9bb8de9ff6b33843f531885b5cbaea352a6270e4
Davis <davis.alexandre@gmail.com>
2025-09-08 10:26:21 -0300

    feat: add CreateUserUseCase

--- index.js
+++ index.js
@@ -1,103 +1,15 @@
 import 'dotenv/config.js'
 import express from 'express'
-import {
-    makeCreateUserController,
-    makeDeleteUserController,
-    makeGetUserBalanceController,
-    makeGetUserByIdController,
-    makeUpdateUserController,
-} from './src/factories/controllers/user.js'
-import {
-    makeCreateTransactionController,
-    makeDeleteTransactionController,
-    makeGetTransactionsByUserIdController,
-    makeUpdateTransactionController,
-} from './src/factories/controllers/transaction.js'
+
+import { PostgresHelper } from './src/db/postgres/helper.js'
 
 const app = express()
 
-app.use(express.json())
-
-app.get('/api/users/:userId', async (request, response) => {
-    const getUserByIdController = makeGetUserByIdController()
-
-    const { statusCode, body } = await getUserByIdController.execute(request)
-
-    response.status(statusCode).send(body)
+app.get('/', async (req, res) => {
+    const result = await PostgresHelper.query('SELECT * FROM users')
+    res.send(JSON.stringify(result))
 })
 
-app.get('/api/users/:userId/balance', async (request, response) => {
-    const getUserBalanceController = makeGetUserBalanceController()
+app.listen(process.env.PORT, () => console.log('listening on port 3000'))
 
-    const { statusCode, body } = await getUserBalanceController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.post('/api/users', async (request, response) => {
-    const createUserController = makeCreateUserController()
-
-    const { statusCode, body } = await createUserController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.patch('/api/users/:userId', async (request, response) => {
-    const updateUserController = makeUpdateUserController()
-
-    const { statusCode, body } = await updateUserController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.delete('/api/users/:userId', async (request, response) => {
-    const deleteUserController = makeDeleteUserController()
-
-    const { statusCode, body } = await deleteUserController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.get('/api/transactions', async (request, response) => {
-    const getTransactionsByUserIdController =
-        makeGetTransactionsByUserIdController()
-
-    const { statusCode, body } =
-        await getTransactionsByUserIdController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.post('/api/transactions', async (request, response) => {
-    const createTransactionController = makeCreateTransactionController()
-
-    const { statusCode, body } = await createTransactionController.execute(
-        request,
-    )
-
-    response.status(statusCode).send(body)
-})
-
-app.patch('/api/transactions/:transactionId', async (request, response) => {
-    const updateTransactionController = makeUpdateTransactionController()
-
-    const { statusCode, body } = await updateTransactionController.execute(
-        request,
-    )
-
-    response.status(statusCode).send(body)
-})
-
-app.delete('/api/transactions/:transactionId', async (request, response) => {
-    const deleteTransactionController = makeDeleteTransactionController()
-
-    const { statusCode, body } = await deleteTransactionController.execute(
-        request,
-    )
-
-    response.status(statusCode).send(body)
-})
-
-app.listen(process.env.PORT, () =>
-    console.log(`Listening on port ${process.env.PORT}`),
-)
+localhost:4500
---
+++ src/factories/controllers/transaction.js
@@ -0,0 +1,18 @@
+export function makeGetTransactionsByUserIdController() {
+    return { execute: async () => ({ statusCode: 501, body: { message: 'Not implemented' } }) }
+}
+
+export function makeCreateTransactionController() {
+    return { execute: async () => ({ statusCode: 501, body: { message: 'Not implemented' } }) }
+}
+
+export function makeUpdateTransactionController() {
+    return { execute: async () => ({ statusCode: 501, body: { message: 'Not implemented' } }) }
+}
+
+export function makeDeleteTransactionController() {
+    return { execute: async () => ({ statusCode: 501, body: { message: 'Not implemented' } }) }
+}
+
+
+
